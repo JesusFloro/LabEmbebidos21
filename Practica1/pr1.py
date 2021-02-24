71f1183dd70514c0606e07ef6d2b13514cad5cfb
JesusFloro <36900866+JesusFloro@users.noreply.github.com>
2021-02-24 11:46:52 -0600

    Add files via upload

--- Practica1/pr1.py
+++ Practica1/pr1.py
@@ -9,7 +9,7 @@ import random
 # Connect the Grove Temperature Sensor to analog port A0
 # SIG,NC,VCC,GND
 sensor = 0
-button = 4      #Port for Button
+BOTON = 3   #Port for Button
 
 # Setting the RGB LCD Screen
 setRGB(0,255,0)
@@ -20,29 +20,33 @@ setText("Random colors")
 for i in range(0,51):
     setRGB(random.randint(0,255),random.randint(0,255),random.randint(0,255))
     time.sleep(.1)
-time.sleep(1)
+  
+#time.sleep(1)
+setText("Presiona el BOTON")
+temp = grovepi.temp(sensor,'1.1')
+setRGB(0,0,255) #RED
+print("temp =", temp)
+#setText(temp)
 
-# ascii char 255 is the cursor character
-setRGB(255,255,255)
-setText(chr(255)*32)
-time.sleep(2)
-          
-pinMode(button,"INPUT")     # Assign mode for Button as input
+print("ENTRAR AL WHILE")        
+pinMode(BOTON,"INPUT")     # Assign mode for Button as input
+
 while True:
-    try:
-        button_status= digitalRead(button)  #Read the Button status
-        if button_status:#If the Button is in HIGH position, show the temperature on the screen with a green bg
-            temp = grovepi.temp(sensor,'1.1')
-            toWrite = "Temp = " + str(int(temp)) + " C"
-            setText(toWrite)
-            setRGB(0,255,0)
-            time.sleep(.5)
-        else: #If Button is in Off position, show a message on the screen with a red bg
-            setText("Presiona el boton") #Quiza anañadir un \n
-            setRGB(255,0,0)
-            time.sleep(.5)
-    except KeyboardInterrupt:   # Stop the buzzer before stopping
-        #digitalWrite(buzzer_pin,0)
-        break
-    except (IOError,TypeError) as e:
-        print("Error")
+    print("while")
+    print("TRY")
+    button_status= digitalRead(BOTON)  #Read the Button status
+    if button_status:   #If the Button is in HIGH position, run the program
+        print("IF")
+        #digitalWrite(buzzer_pin,1)                     
+        # print "\tBuzzing"
+        temp = grovepi.temp(sensor,'1.1')
+        #print("temp =", temp)
+        time.sleep(.5)
+        setText("TEMP = " + str(int(temp)))
+        setRGB(0,255,0)#GREEN
+        time.sleep(.5)
+    else:
+        print("ELSE")
+        setText("Presiona el boton")
+        setRGB(255,0,0) #RED
+        time.sleep(.5)
